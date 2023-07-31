428edf72f3c330cc5e70f94f179219024ff7523f
Chris Bentley <chrisrbentley@pm.me>
2023-07-31 13:15:16 -0400

    Implemented Hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+	return (
+		<section
+			className="p-12 m-6
+		"
+		>
+			<h1 className="text-5xl p-2">Christopher Bentley</h1>
+			<h2 className="text-3xl p-2">Frontend Developer</h2>
+		</section>
+	);
+};
+
+export default Hero;
