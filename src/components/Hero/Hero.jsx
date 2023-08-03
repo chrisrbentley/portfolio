e07b0c1267a453a0e94fbf9a60006daa69ce8be6
Chris Bentley <chrisrbentley@pm.me>
2023-08-03 18:28:07 -0400

    Included Hero in header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,12 @@
 const Hero = () => {
 	return (
-		<section
+		<heading
 			className="p-12
 		"
 		>
 			<h1 className="text-5xl p-2">Christopher Bentley</h1>
 			<h2 className="text-3xl p-2">Frontend Developer</h2>
-		</section>
+		</heading>
 	);
 };
 
